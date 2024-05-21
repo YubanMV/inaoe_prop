7bcece68cf7e72cbe95d3f604ccbd79c5c002064
yubanmv <yuban.vasquez@gmail.com>
2024-05-20 22:37:29 -0600

    Actualizacion de programas

--- tarea2Ejercicio1Yuban.py
+++ tarea2Ejercicio1Yuban.py
@@ -9,22 +9,27 @@ def calcular_descuento_y_total(consumo, area):
     total_a_pagar = consumo - descuento
     return descuento, total_a_pagar
 
-while True:
-    try:
-        consumo = float(input("Introduce el monto total del consumo: $"))
-        if consumo < 0:
-            raise ValueError("El monto total del consumo no puede ser negativo.")
-        
-        area = input("Introduce el área del cliente (1, 2, 3): ")
-        if area not in ["1", "2", "3"]:
-            raise ValueError("Área no válida. Por favor, introduce un área válida (1, 2, 3).")
-        
-        descuento, total_a_pagar = calcular_descuento_y_total(consumo, area)
+def main():
+    while True:
+        try:
+            consumo = float(input("Introduce el monto total del consumo: $"))
+            if consumo < 0:
+                raise ValueError("El monto total del consumo no puede ser negativo.")
+            
+            area = input("Introduce el área del cliente (1, 2, 3): ")
+            if area not in ["1", "2", "3"]:
+                raise ValueError("Área no válida. Por favor, introduce un área válida (1, 2, 3).")
+            
+            descuento, total_a_pagar = calcular_descuento_y_total(consumo, area)
 
-        print(f"Descuento aplicado: ${descuento:.2f}")
-        print(f"Total a pagar: ${total_a_pagar:.2f}")
-        
-        break
-    except ValueError as e:
-        print(e)
-        print("Inténtalo de nuevo.")
+            print(f"Descuento aplicado: ${descuento:.2f}")
+            print(f"Total a pagar: ${total_a_pagar:.2f}")
+            
+            break
+        except ValueError as e:
+            print(e)
+            print("Inténtalo de nuevo.")
+
+
+if __name__ == "__main__":
+     main()
